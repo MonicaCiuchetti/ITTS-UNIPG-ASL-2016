22c4be44e403198fe270e49ea9ca1f55360bd789
Amedeo Di Gaetano <digaetano.amedeo@gmail.com>
2017-02-20 15:47:39 +0100

    aggiunte modifiche scriptVersione4.py

--- scriptVersione4.py
+++ scriptVersione4.py
@@ -6,19 +6,25 @@ from datetime import datetime
 campiJSON = { "id", "utc_timestamp"}
 oldLines = 0														
 lastEditTxt = os.stat("temperature.txt").st_mtime
-#fileBackup = open(strftime("%Y-%m-%d-%H:00.json", gmtime()),"a")
-giorno = datetime.now().hour
-path = "/home/xubuntu/" + strftime("%Y-%m-%d-%H",gmtime())
+giorno = datetime.now().day
+ora = datetime.now().hour
+path = os.environ('HOME') + "/" + strftime("%Y-%m-%d",gmtime())
 os.mkdir(path)
+virgola = False
+
+schema = avro.schema.parse(open("schema-prova.avsc", "rb").read())
 					
 while True:
 	newEditTxt = os.stat("temperature.txt").st_mtime
-	if giorno != datetime.now().hour:
-		giorno = datetime.now().hour
-		path = "/home/xubuntu/" + strftime("%Y-%m-%d-%H",gmtime())
+	if giorno != datetime.now().day:
+		giorno = datetime.now().day
+		path = os.environ('HOME') + "/" + strftime("%Y-%m-%d",gmtime())
 		os.mkdir(path)
-	#	fileBackup.close()
-	fileBackup = open(strftime(path + "/%Y-%m-%d-%H:%M.json", gmtime()),"a")
+	if ora != datetime.now().hour:
+		virgola = False
+		ora = datetime.now().hour
+	fileBackup = open(strftime(path + "/%Y-%m-%d-%H:00.json", gmtime()),"a")
+	writer = DataFileWriter(open(strftime(path + "/%Y-%m-%d-%H:00.avro", gmtime()), "ab"), DatumWriter(), schema)
 	if newEditTxt > lastEditTxt:
 		fileTxt = open("temperature.txt", "r")
 		data = fileTxt.readlines()
@@ -27,18 +33,24 @@ while True:
 			riga = data[oldLines]
 			riga.replace("\n", "")
 			lista = riga.split("   ")
+			if virgola:
+				fileBackup.write(",")
+			else:
+				virgola = True
 			oggettoJSON = "{ "
 			i = 0
 			for campo in campiJSON:
 				oggettoJSON = oggettoJSON + "\"" + campo + "\": \"" + str(lista[i]).replace("\n", "") + "\","
 				i = i + 1
 			oggettoJSON = oggettoJSON[:-1]
-			oggettoJSON = oggettoJSON + " },"
+			oggettoJSON = oggettoJSON + " }"
 			fileBackup.write(oggettoJSON)
+			writer.append(json.loads(oggettoJSON))
 			oldLines = oldLines + 1
 		lastEditTxt = newEditTxt
 		fileTxt.close()
 		fileBackup.close()
+		writer.close()
 		time.sleep(5)
 
 		
